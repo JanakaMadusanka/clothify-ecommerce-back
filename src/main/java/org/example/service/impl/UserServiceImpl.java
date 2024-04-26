cd4aa0c6cf6530498b8da4a33f9cb9956e477b40
janaka <jnk.madusanka86@gmail.com>
2024-04-26 07:04:34 +0530

    modelMapper config file added

--- src/main/java/org/example/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -14,12 +14,7 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
     final UserRepository repository;
-    ModelMapper mapper;
-
-    @Bean
-    public void setup(){
-        this.mapper = new ModelMapper();
-    }
+    final ModelMapper mapper;
 
     @Override
     public void addUser(User user) {
